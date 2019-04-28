e196df24ddf02ffca1c86910b709924eb5fc2e43
JeremyLezmy <49833410+JeremyLezmy@users.noreply.github.com>
2019-04-28 23:11:44 +0200

    Update tp.py

--- tp.py
+++ tp.py
@@ -8,10 +8,12 @@ Created on Sun Apr 21 16:32:25 2019
 from pylab import*
 from numpy import polynomial as P
 import numpy as np
+import matplotlib.pyplot as plt
 #############Lecture des fichiers#############,r"D:\Téléchargement\tp2019-1",r"D:\Téléchargement\tp2019-2",r"D:\Téléchargement\tp2019-3"
 liste_fichiers = [r"D:\Téléchargement\tp2019pb2"]
 liste_size = [2001]
 tt_masses = []
+delta_mu=[]
 tab_dE1=[]
 beta_gamma=[]
 vtest=[]
@@ -39,11 +41,12 @@ for rr in range(0, len(liste_fichiers)):
              col1.append(float(tab[1]))
              col2.append(float(tab[2]))
              col3.append(float(tab[3]))
-             
+             col4.append(float(tab[4]))
     
          tab_mini=[]
          tab_largeur = []
          tt=[]
+         vv=[]
          ############Calcul du temps moyen#################
          for col in [col1, col2, col3]:
              indice_mini=0
@@ -69,12 +72,12 @@ for rr in range(0, len(liste_fichiers)):
              vtest.append(v_m)        
              
              tt.append(t_m[v_m.index(min(v_m))])
-            
+             vv.append(min(v_m))
             
              ##############Calcul des vitesses############
-         if tt[0]<tt[1] and tt[1]<tt[2] and (tt[1]-tt[0])<(tt[2]-tt[1]):
-             beta1 = (22.5*10**(-2))/(1.*c*(tt[1]-tt[0]))
-             beta2 = (5.5*10**(-2) )/(1.*c*(tt[2]-tt[1]-(13.5*10**(-2)/(beta1*c))))                   ###ATTENTION MODIF
+         if tt[0]<tt[1] and tt[1]<tt[2] :
+             beta1 = (22.75*10**(-2))/(1.*c*(tt[1]-tt[0]))
+             beta2 = (5.5*10**(-2) )/(1.*c*(tt[2]-tt[1]-(17.5*10**(-2)/(beta1*c))))                   ###ATTENTION MODIF
              beta3 = beta1
         
         
@@ -112,15 +115,15 @@ for rr in range(0, len(liste_fichiers)):
                  if mc2>0:
                      tt_masses.append(mc2)
                  ############Calcul des erreurs############
-                 delta_x1=2.
-                 delta_x2=2.
-                 delta_t1=1.*10**-10
-                 delta_t2=1.*10**-10
-                 delta_dx = 1
-                 d1 = 66.5*10**-1
-                 d2 = 73.*10**-1
-                 delta_beta1=(beta1*((delta_x1/d1)+(delta_t1/(tt[1]-tt[0]))))
-                 delta_beta2=(beta2*((delta_x2/d2)+(delta_t2/(tt[2]-tt[1]))))       #######ATTENTION MODIF
+                 delta_x1=0.2
+                 delta_x2=0.2
+                 delta_t1=1*10**-10
+                 delta_t2=1*10**-10
+                 delta_dx = 0.2
+                 d1 = 22.75
+                 d2 = 17.5
+                 delta_beta1=(beta1*((delta_x1/d1)+(2*delta_t1/(tt[1]-tt[0]))))
+                 delta_beta2=(beta2*((delta_x2/d2)+(2*delta_t2/(tt[2]-tt[1]-(11.75*10**(-2)/(beta1*c))))))       #######ATTENTION MODIF
     
                  delta_gamma1=(gamma1**3*beta1*delta_beta1)
                 
@@ -137,14 +140,17 @@ for rr in range(0, len(liste_fichiers)):
                  f8=K*z**2*Z*delta_gamma1/(A*beta1**2*gamma1)
                  f9=K*z**2*Z*delta_T_max/(2.*A*beta1**2*T_max)
                 
-                 delta_dE1 = f6*f7+f8+f9
+                 #delta_dE1 = f6*f7+f8+f9
+                 delta_dE1 = dE1 * (delta_dx/dx) + 2*(delta_beta1/beta1) * abs(-dE1 + 6.24*10**12*dx*10**(-2)*(4*np.pi/(9.11*10**(-31)*9*10**16))*(Z*11.35*10**3*6.022*10**(23)/(A*10**(-3)*beta1**2))*((((1.6*10**-19)**2)/(4*np.pi*8.854*10**-12))**2)*((gamma1/beta1)**2 -1))
                  
                  #print("beta1=",beta1,"beta2=",beta2,"gamma1=",gamma1,"gamma2=",gamma2,"dE1=",dE1,"T_max=",T_max,"masse=",mc2)
                 
                 
-                 delta_m = mc2 *(delta_dE1/dE1 + 10**-1*delta_dx/dx+(delta_gamma1+delta_gamma2)/(gamma1-gamma2))
-                
+                 #delta_m = mc2 *(delta_dE1/dE1 + 10**-1*delta_dx/dx+(delta_gamma1+delta_gamma2)/(gamma1-gamma2))
+                 delta_m = delta_dE1/(gamma1 -gamma2) + dE1*((delta_gamma1+delta_gamma2)/(gamma1-gamma2)**2)
+                 delta_mu.append(delta_m)
                  #print("delta_beta1=",delta_beta1,"delta_beta2=",delta_beta2,"delta_gamma1=",delta_gamma1,"delta_gamma2=",delta_gamma2,"delta_dE1=",delta_dE1,"delta_T_max=",delta_T_max,"delta_m=",delta_m)
                  nb = nb+ 1
     
+                
                 
